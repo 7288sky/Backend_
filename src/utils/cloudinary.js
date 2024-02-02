fbb0714d42c5e6eea4ce927132ea2c1a9074cff8
7288sky <123364227+7288sky@users.noreply.github.com>
2024-02-02 11:56:27 +0530

    Cloudinary and middlewares

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary=async(localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+        // upload the file on cloudinary
+       const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // FILE HAS BEEN UPLOADED UPLOADED
+        console.log("File is uploaded on cloudinary",response.url)
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath) // Remove the locally saved temporary files
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary}
