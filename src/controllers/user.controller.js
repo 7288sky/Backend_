931aa4d33fae3fcc380ba84b534133858cc0e7f3
7288sky <123364227+7288sky@users.noreply.github.com>
2024-02-08 13:39:08 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -254,7 +254,7 @@ return res.status(200)
 const getCurrentUser=asyncHandler(async(req,res)=>{
   return res
   .status(200)
-  .json(200,req.user,"current user fetched successfully")
+  .json(new ApiResponse(200,{},"Password changed successfully"))
 })
 
 const updateAccountDetails=asyncHandler(async(req,res)=>{
